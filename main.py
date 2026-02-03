1eb255a71c2e3926afb325751aeb205d192a7fa9
HermanLiuDev <leonardliu1@gmail.com>
2026-02-03 16:14:02 +0800

    main struction

--- main.py
+++ main.py
@@ -1,27 +1,87 @@
 import logging
 
+
+
 class ReceiptRAGSystem:
-    #加载配置文件
+    '''
+    食谱检索系统
+    '''
+
+    def __init__(self, config：dict = None):
+        '''
+        系统变量初始化
+        
+        Args：
+        config: 配置文件
+
+        '''
+        #加载配置文件
+        self.config = config
+        
+        #初始化数据处理、检索构建、检索优化和答案生成模块
+        self.data_model = None
+        self.index_model = None
+        self.retrieval_model = None
+        self.generation_model = None
+
+    def init_system(self):
+        '''
+        初始化系统模块
+        
+        '''
+        #加载数据处理模块
+
+        #加载索引模块
+
+        #加载检索模块
+
+        #加载生成模块
+        pass
+
+    def bulid_knowledge_database(self):
+        '''
+        构建知识库
+
+        '''
+        #获取文档
+
+        #处理文档和切块
+
+        #构建索引
+
+        #初始化检索模块
+
+        #数据库统计信息
+        pass
+
+    def answer_question(self, question: str):
+        '''
+        回答用户问题
+
+        Args:
+        question: 用户输入的问题
+
+        Returns:
+        answer: 系统生成的回答
+        '''
+
+        #用户问题与处理
 
 
-    #初始化各模块
+        #实施检索
 
 
-    #加载索引与系统就绪
+        #生成回答
 
 
-    #用户问题与处理
+        #返回结果
+        pass
 
-
-    #实施检索
-
-
-    #生成回答
-
-
-    #返回结果
-
-
+    def run_interactive(self):
+        '''
+        运行交互式问答系统
+        
+        '''
 
 
 def main():
